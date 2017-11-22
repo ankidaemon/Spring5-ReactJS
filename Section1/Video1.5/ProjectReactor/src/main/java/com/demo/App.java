87f2e2e020163654c0305ca041076dac0fa667fa
Ankit Mishra <ankitmishra1701@gmail.com>
2017-11-22 19:05:02 +0530

    cleanup

--- Section1/Video1.5/ProjectReactor/src/main/java/com/demo/App.java
+++ Section1/Video1.5/ProjectReactor/src/main/java/com/demo/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PublisherExample.fluxSubscriber();
-		//PublisherExample.monoSubscriber();
+		PublisherExample.monoSubscriber();
 	}
 
 }
--- Section1/Video1.5/ProjectReactor/src/main/java/com/demo/PublisherExample.java
+++ Section1/Video1.5/ProjectReactor/src/main/java/com/demo/PublisherExample.java
@@ -1,7 +1,6 @@
 package com.demo;
 
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,19 +8,19 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class PublisherExample {
-	static void fluxSubscriber(){
-		Flux<Integer> seq1 = Flux.range(1, 3);
-		List<String> iterable = Arrays.asList("foo", "bar", "foobar");
-		Flux<String> seq2 = Flux.fromIterable(iterable);
-		seq1.subscribe(i -> System.out.println(i));
-		seq2.subscribe(i -> System.out.println(i));
-		
-		Flux<String> flux = Flux.just("{A}", "{B}", "{C}");
-		Flux<String> intervalFlux3 = Flux
-									.interval(Duration.ofMillis(500))
-									.zipWith(flux, (i, item) -> "item " + i + ": " + item);
-		
-		intervalFlux3.subscribe(System.out::println);
+	static void fluxSubscriber() {
+		Flux<Integer> flux1 = Flux.range(4, 10);
+
+		List<String> iterable = Arrays.asList("Hello", "World", "!");
+		Flux<String> flux2 = Flux.fromIterable(iterable);
+
+		flux1.subscribe(i -> System.out.println(i));
+		flux2.subscribe(i -> System.out.println(i));
+
+		Flux<String> intervalFlux = Flux.interval(Duration.ofMillis(500)).zipWith(flux2,
+				(i, item) -> "item " + i + ": " + item);
+
+		intervalFlux.subscribe(System.out::println);
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
@@ -29,16 +28,12 @@ public class PublisherExample {
 			e.printStackTrace();
 		}
 	}
-	
-	static void monoSubscriber(){
-		Mono<String> noData = Mono.empty(); 
-		Mono<String> data = Mono.just("foo");
-		data.subscribe(i -> System.out.println(i),
-			    error -> System.err.println("Error " + error),
-			    () -> {System.out.println("Done");});
-		
-		/*Error signals and completion signals are both terminal events and are exclusive of one another (you never get both). To make the completion consumer work, we must take care not to trigger an error.
 
-		The completion matcher is a pair of empty parentheses because it matches the run method in the Runnable interface, which has no parameters. The preceding code produces the following output:*/
+	static void monoSubscriber() {
+		Mono<String> noData = Mono.empty();
+		Mono<String> data = Mono.just("Hello");
+		data.subscribe(i -> System.out.println(i), error -> System.err.println("Error " + error), () -> {
+			System.out.println("Done");
+		});
 	}
 }
