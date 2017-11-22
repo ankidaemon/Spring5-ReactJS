d991754a6fe4d6c57b4e743561ceb730832a5639
Ankit Mishra <ankitmishra1701@gmail.com>
2017-11-22 23:45:36 +0530

    cleanup

--- Section2/Video 2.4/ReactiveWebClient/src/main/java/com/demo/config/ReactiveWebClientApplication.java
+++ Section2/Video 2.4/ReactiveWebClient/src/main/java/com/demo/config/ReactiveWebClientApplication.java
@@ -44,54 +44,71 @@ public class ReactiveWebClientApplication {
 
 	private void get(WebClient webClient) {
 		// TODO Auto-generated method stub
-		Mono<User> result = webClient.get().uri("/Users/{id}", 1).accept(MediaType.APPLICATION_JSON).retrieve()
+		Mono<User> result = webClient.get().uri("/{id}", 1).accept(MediaType.APPLICATION_JSON).retrieve()
 				.bodyToMono(User.class);
+		
+		result.subscribe(System.out::println);
 
-		Flux<User> fluxResult = webClient.get().uri("/Users").accept(MediaType.TEXT_EVENT_STREAM).retrieve()
+		/*Flux<User> fluxResult = webClient.get().uri("/").accept(MediaType.TEXT_EVENT_STREAM).retrieve()
 				.bodyToFlux(User.class);
+		
+		fluxResult.subscribe(System.out::println);*/
 
-		Mono<User> monoResult = webClient.get().uri("/persons/{id}", 1).accept(MediaType.APPLICATION_JSON).retrieve()
+		Mono<User> monoResult = webClient.get().uri("/{id}", 1).accept(MediaType.APPLICATION_JSON).retrieve()
 				.onStatus(HttpStatus::is4xxClientError, (ClientResponse clientResponse) -> {
-					throw new WebClientResponseException("Access Forbidden", 0, null, null, null, null);
+					throw new WebClientResponseException("Bad Request", 0, null, null, null, null);
 				}).onStatus(HttpStatus::is5xxServerError, (ClientResponse clientResponse) -> {
-					throw new WebClientResponseException("Access Forbidden", 0, null, null, null, null);
+					throw new WebClientResponseException("Internal Server Error", 0, null, null, null, null);
 				}).bodyToMono(User.class);
 
-		monoResult = (Mono<User>) webClient.get().uri("/persons/{id}", 1).accept(MediaType.APPLICATION_JSON).exchange()
+		monoResult.subscribe(System.out::println);
+		
+		monoResult = (Mono<User>) webClient.get().uri("/{id}", 1).accept(MediaType.APPLICATION_JSON).exchange()
 				.flatMap(response -> response.bodyToMono(User.class));
 
 		Mono<ResponseEntity<User>> resultResponseEntity = (Mono<ResponseEntity<User>>) webClient.get()
-				.uri("/persons/{id}", 1).accept(MediaType.APPLICATION_JSON).exchange()
+				.uri("/{id}", 1).accept(MediaType.APPLICATION_JSON).exchange()
 				.flatMap(response -> response.toEntity(User.class));
+		
+		resultResponseEntity.subscribe(System.out::println);
 	}
 
 	private void post(WebClient webClient) {
 		// TODO Auto-generated method stub
-		Mono<User> UserMono = null;
+		/*Mono<User> UserMono = null;
 
-		Mono<Void> result = webClient.post().uri("/Users/{id}", 1).contentType(MediaType.APPLICATION_JSON)
+		Mono<Void> result = webClient.post().uri("/create").contentType(MediaType.APPLICATION_JSON)
 				.body(UserMono, User.class).retrieve().bodyToMono(Void.class);
 
 		Flux<User> UserFlux = null;
 
-		result = webClient.post().uri("/Users/{id}", 1).contentType(MediaType.APPLICATION_STREAM_JSON)
-				.body(UserFlux, User.class).retrieve().bodyToMono(Void.class);
+		result = webClient.post().uri("/create").contentType(MediaType.APPLICATION_STREAM_JSON)
+				.body(UserFlux, User.class).retrieve().bodyToMono(Void.class);*/
 
-		User User = null;
+		User user = new User();
+		user.setUserName("BatMan");
+		user.setUserId(1);
+		user.setPhone("1234567890");
 
-		result = webClient.post().uri("/Users/{id}", 1).contentType(MediaType.APPLICATION_JSON).syncBody(User)
+		Mono<Void> result1 = webClient.post().uri("/create").contentType(MediaType.APPLICATION_JSON).syncBody(user)
 				.retrieve().bodyToMono(Void.class);
+		result1.subscribe(System.out::println);
 
 	}
 
 	private void delete(WebClient webClient) {
 		// TODO Auto-generated method stub
-
+		webClient.delete().uri("/delete/{id}",1).exchange().subscribe();
 	}
 
 	private void put(WebClient webClient) {
 		// TODO Auto-generated method stub
-
+		User user=new User();
+		user.setUserName("Joker");
+		user.setUserId(1);
+		user.setPhone("1234567890");
+		webClient.put().uri("/update").contentType(MediaType.APPLICATION_JSON).syncBody(user)
+		.retrieve().bodyToMono(User.class).subscribe(System.out::println);
 	}
 
 }
