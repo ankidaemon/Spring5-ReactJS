c892b7b8861e5b99bc267e485d915090d6f26d2b
Ankit Mishra <ankitmishra1701@gmail.com>
2018-01-06 19:49:05 +0530

    Request Scope

--- Section1/Video1.6/Spring-Reactive/src/main/java/com/demo/model/User.java
+++ Section1/Video1.6/Spring-Reactive/src/main/java/com/demo/model/User.java
@@ -1,8 +1,11 @@
 package com.demo.model;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Scope(scopeName = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 @Document
 public class User {
 	
